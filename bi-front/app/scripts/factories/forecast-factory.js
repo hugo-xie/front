8bc8b7b9adea0c54e27f7bc3c3a866868b628f6b
hugo-xie <m15955038579@163.com>
2016-03-12 20:14:03 +0800

    跨年，修复bug

--- bi-front/app/scripts/factories/forecast-factory.js
+++ bi-front/app/scripts/factories/forecast-factory.js
@@ -33,4 +33,18 @@ angular.module('nevermore')
 
 	return this;
 
+	})
+	.factory('forecastFactory_gdp',function($resource,BASE_URL){
+		this.query=function(headers){
+			return $resource(BASE_URL+'/predictData/:tableName/list?year=:year',{
+          tableName:'@tableName',
+          year:'@year'
+			},{
+				'get':{
+					method:'GET',
+					headers:headers
+				}
+			});
+		};
+		return this;
 	});
